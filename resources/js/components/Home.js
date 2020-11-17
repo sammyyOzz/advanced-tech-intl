abe3fee801170cdb1ddf16b26ac62614a9379c2d
Sammy <sammyoziegbe@gmail.com>
2020-11-17 19:10:29 +0100

    started 'our contributions' section

--- resources/js/components/Home.js
+++ resources/js/components/Home.js
@@ -2,12 +2,13 @@ import React from 'react'
 import './Home.css'
 import HomeTopCard from './HomeTopCard'
 import Grid from '@material-ui/core/Grid';
+import { Button } from '@material-ui/core';
 
 function Home() {
     return (
         <div className="home">
             <div className="home__top">
-                <div className="home__topOverlay">
+                <div className="home__overlay">
                     {/* <div>
                         <button>Click me</button>
                     </div> */}
@@ -39,7 +40,7 @@ function Home() {
 
             <Grid container>
                 <Grid item md={3} />
-                <Grid item md={6}>
+                <Grid item xs={12} md={6}>
                     <div className="home__aboutUs">
                         <h3>About Us</h3>
                         <h2>25 Long Years Of Service Excellence With Quality At It's Best!</h2>
@@ -50,7 +51,91 @@ function Home() {
                 <Grid item md={3} />
             </Grid>
 
-            
+            <div className="home__ourProducts">
+                <div className="home__overlayProducts">
+                    <div className="home__productsContent">
+                        <Grid container spacing={8}>
+                            <Grid item xs={12} md={7}>
+                                <div className="home__productsLeft">
+                                    <h3 style={{ marginBottom: '30px', color: '#b0bec5'}}>Our products</h3>
+                                    <h2>We are providing the most reliable and trendy solution you are looking for.</h2>
+
+                                    <hr/>
+
+                                    <h3 className="leftHoverBorder">Industrial Automation</h3>
+                                    <div className="leftLinksBox">
+                                        <span id="firstSpanLink">Material Testing</span>
+                                        <span>Fork Testing</span>
+                                        <span>Torque Testing</span>
+                                        <span>Primary Flow Calibrator</span>
+                                        <span>Networking & Enterprise Solution</span>
+                                        <span>Sensing and Measuting Instruments</span>
+                                        <span>Indicator & Controller</span>
+                                        <span>Date Acquisition and Control System</span>
+                                        <span id="lastSpanLink">Environmental Monitoring</span>
+                                    </div>
+
+                                    <hr/>
+
+                                    <h3 className="leftHoverBorder">SCADA & Analytics</h3>
+                                    <div className="leftLinksBox">
+                                        <span id="firstSpanLink">Enterprise Solution</span>
+                                        <span>Data Historian</span>
+                                        <span>IoT Solutions</span>
+                                        <span>Mobility Solution</span>
+                                        <span>Visualization</span>
+                                        <span id="lastSpanLink">Analytics</span>
+                                    </div>
+
+                                    <hr/>
+
+                                    <h3 className="leftHoverBorder">Education & Research</h3>
+                                    <div className="leftLinksBox">
+                                        <span id="firstSpanLink">Aeronautical Engineering</span>
+                                        <span>Civil Engineering</span>
+                                        <span>Mechanical Engineering</span>
+                                        <span>Steam & Renewable Energy</span>
+                                        <span>Material Testing & Research</span>
+                                        <span id="lastSpanLink">Corrosion Online</span>
+                                    </div>
+
+                                    <hr/>
+
+                                    <h3 className="leftHoverBorder">Defense & Aerospace</h3>
+                                    <div className="leftLinksBox">
+                                        <span id="firstSpanLink">Test & Measurement</span>
+                                        <span>Data Center Management</span>
+                                        <span>COSA Embedded Systems</span>
+                                        <span>Rugged Displays & Panel PC</span>
+                                        <span>Motion Simulation Systems</span>
+                                        <span>Single Board Computers</span>
+                                        <span id="lastSpanLink">Chassis, Components & Power Supplies</span>
+                                    </div>
+
+                                    <hr/>
+
+                                </div>
+                            </Grid>
+
+
+                            <Grid item xs={12} md={5}>
+                                <div className="home__productsRight">
+                                    <div className="home__productsRightContent">
+                                        <h2 style={{ marginBottom: '40px'}}>The only place where you’ll get the perfect solution for all your industry needs.</h2>
+                                        <Button variant="contained" style={{ backgroundColor: '#fdd835', color: 'white'}}><h4>Learn More</h4></Button>
+                                    </div>
+                                </div>
+                            </Grid>
+                        </Grid>
+                    </div>
+                </div>
+            </div>
+
+            <div className="home__ourContributions">
+                <h4>Our Contributions</h4>
+                <h2>Industries We Serve!</h2>
+                <p>We help Industries to optimise with Integration of data from Shop floor to Top floor Realtime to make the timely decision.</p>
+            </div>
         </div>
 
     )
